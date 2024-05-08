e1b427588577c8d207f48ab3f19444dc84f8868a
Buonarroti <mattia.bronzetti@buonarroti.tn.it>
2024-05-08 11:20:40 +0200

    created homepage and connection to db

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+    <header class="text-center py-20">
+        <h1 class="text-5xl font-bold italic text-black mt-20 mb-20">
+            Bella Scaloppa Restaurant
+        </h1>
+
+        <a href="menu.php">
+            <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg">
+                Order Now
+            </button>
+        </a>
+    </header>
+</body> 
+</html>
---
+++ menu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Menu</title>
+</head>
+<body>
+<ul class="menu bg-base-200 w-56 rounded-box">
+  <li><a>Item 1</a></li>
+  <li><a>Item 2</a></li>
+  <li><a>Item 3</a></li>
+</ul>
+</body>
+</html>
